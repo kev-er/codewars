430b4d1c9c1346c1521625e8b7f3583bec8f5470
Kevin <holmk21@gmail.com>
2023-03-13 18:10:33 +0000

    daily codewars

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log('Check!')
+
+// DAY 1 ---> STRINGS 8kyu
+
+function areYouPlayingBanjo(name) {
+    if (name [0].toLowerCase() === 'r') {
+        return name + ' plays banjo'; 
+    } else {
+        return name + ' does not play banjo'; 
+    }
+}
+
+
+  const chai = require("chai");
+  const assert = chai.assert;
+  chai.config.truncateThreshold=0;
+  
+  describe("Basic tests", () => {
+    it("Testing for fixed tests", () => {
+      assert.strictEqual(areYouPlayingBanjo("Adam"), "Adam does not play banjo");
+      assert.strictEqual(areYouPlayingBanjo("Paul"), "Paul does not play banjo");
+      assert.strictEqual(areYouPlayingBanjo("Ringo"), "Ringo plays banjo");
+      assert.strictEqual(areYouPlayingBanjo("bravo"), "bravo does not play banjo");
+      assert.strictEqual(areYouPlayingBanjo("rolf"), "rolf plays banjo");
+      })
+    })
+  
