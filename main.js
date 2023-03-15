b07805fbb11aae3a002a4b5581604f0130337913
Kevin <holmk21@gmail.com>
2023-03-15 19:31:42 +0000

    Daily codewars

--- main.js
+++ main.js
@@ -25,3 +25,23 @@ function areYouPlayingBanjo(name) {
       })
     })
   
+    // Daily ---> 8kyu
+
+  function hoopCount (n) {
+    if (n >= 10) { 
+      return("Great, now move on to tricks") }
+      else { 
+        return ("Keep at it until you get it") }
+}
+    
+    
+     // Test 
+
+    const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(hoopCount(3),"Keep at it until you get it" ) 
+Test.assertEquals(hoopCount(11),"Great, now move on to tricks" )
+  });
+});
